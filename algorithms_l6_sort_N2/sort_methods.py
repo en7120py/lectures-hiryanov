83bdb5fd5528f13d7cdea9db0b7f13e879244d0c
inity8 <di848588@gmail.com>
2020-08-04 00:30:43 +0300

    add insert_sort

--- algorithms_l6_sort_N2/sort_methods.py
+++ algorithms_l6_sort_N2/sort_methods.py
@@ -4,7 +4,12 @@ def insert_sort(A):
     :param A: list
     :return: ordered array
     """
-    pass
+    N = len(A)
+    for top in range(1, N):
+        k = top
+        while k > 0 and A[k-1] > A[k]:
+            A[k], A[k-1] = A[k-1], A[k]
+            k -= 1
 
 def choise_sort(A):
     """
@@ -24,26 +29,27 @@ def bubble_sort(A):
 
 
 def test_sort(sort_algorithm):
+    print("Testing: ", sort_algorithm.__doc__)
     print("testcase #1: ", end="")
     A = [4, 2, 5, 1, 3]
     A_sorted = [1, 2, 3, 4, 5]
     sort_algorithm(A)
-    print( "ok" if A==A_sorted else "fail")
+    print("ok" if A == A_sorted else "fail")
 
     print("testcase #2: ", end="")
     A = list(range(10,20)) + list(range(0,10))
     A_sorted = list(range(20))
     sort_algorithm(A)
-    print( "ok" if A==A_sorted else "fail")
+    print("ok" if A == A_sorted else "fail")
 
     print("testcase #3: ", end="")
     A = [4, 2, 4, 2, 1]
     A_sorted = [1, 2, 2, 4, 4]
     sort_algorithm(A)
-    print( "ok" if A==A_sorted else "fail")
+    print("ok" if A == A_sorted else "fail")
 
 
 if __name__ == '__main__':
-    test_sort(insert_sort())
-    test_sort(bubble_sort())
-    test_sort(test_sort())
+    test_sort(insert_sort)
+    test_sort(choise_sort)
+    test_sort(bubble_sort)
