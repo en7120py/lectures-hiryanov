43697f30348e4d57097d02d2bffca4d22518a320
inity8 <di848588@gmail.com>
2020-08-03 23:47:48 +0300

    complite

--- algorithms_l5_arrays/sieve_of_eratosthenes.py
+++ algorithms_l5_arrays/sieve_of_eratosthenes.py
@@ -1,16 +1,19 @@
-def sieve(N:int,x:int):
-    pass
-"""
-A=[True]*N
-A[0]=A[1]=False
-for k in range(2,N):
-    if A[k]:
-        for m in range(2*k,N,k):
-            A[m]=False
-for k in range(N):
-    print(k, "-", "prime" if A[k] else "composite")
-
-"""
+def sieve(N:int, x:int):
+    """
+    :param N: range N-1
+    :param x: research number
+    :return: number - "prime" or number - "composite".
+    for 0 and 1 (neutral) - "composite".
+    """
+    A = [True]*N
+    A[0] = A[1] = False
+    for k in range(2, N):
+        if A[k]:
+            for m in range(2*k, N, k):
+                A[m] = False
+    # for k in range(N):
+    print(x, " - ", "prime" if A[x] else "composite")
+    return ("prime" if A[k] else "composite")
 
 
 def test_sieve():
