653de6182fbada74c3eb99490b89445fd4a57da6
inity8 <di848588@gmail.com>
2020-08-03 23:35:26 +0300

    add tests

--- algorithms_l5_arrays/sieve_of_eratosthenes.py
+++ algorithms_l5_arrays/sieve_of_eratosthenes.py
@@ -1,3 +1,5 @@
+def sieve(N:int,x:int):
+    pass
 """
 A=[True]*N
 A[0]=A[1]=False
@@ -9,3 +11,23 @@ for k in range(N):
     print(k, "-", "prime" if A[k] else "composite")
 
 """
+
+
+def test_sieve():
+    n = sieve(10, 8)
+    if n == "composite":
+        print("#test1 - passed")
+    else:
+        print("#test1 - fail")
+    n = sieve(20, 7)
+    if n == "prime":
+        print("#test2 - passed")
+    else:
+        print("#test2 - fail")
+    n = sieve(11, 1)
+    if n == "composite":
+        print("#test2 - passed")
+    else:
+        print("#test2 - fail")
+
+test_sieve()
