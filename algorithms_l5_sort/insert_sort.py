de22ac5b5c35cdacc7999ad37d21126b5108f26d
inity8 <di848588@gmail.com>
2020-08-04 00:00:06 +0300

    add insert sort

---
+++ algorithms_l5_sort/insert_sort.py
@@ -0,0 +1,24 @@
+def insert_sort(A):
+    """
+    Sort list A by inserts
+    :param A: list
+    :return: ordered array
+    """
+    pass
+
+def choise_sort(A):
+    """
+    Sort list A by choise method
+    :param A: list
+    :return: ordered array
+    """
+    pass
+
+def bubble_sort(A):
+    """
+    Sort list A by bubble method
+    :param A: list
+    :return: ordered array
+    """
+    pass
+
