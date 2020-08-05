088b48a94b74d7f395f37d9336af57c83f7ab8d1
inity8 <di848588@gmail.com>
2020-08-06 01:02:20 +0300

    graphics

---
+++ algorithms_l7_recursia/1.py
@@ -0,0 +1,11 @@
+from graphics import *
+win = GraphWin("Russian game", 300, 300)
+
+def fractal_rectangle(A, B, C, D, deep=10):
+    if deep<1:
+        return
+
+if __name__ == '__main__':
+    my_rectangle = Rectangle(Point(2,4),Point(4,8))
+    my_rectangle.draw(win)
+
